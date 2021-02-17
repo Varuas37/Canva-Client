7eb7b4316e69beb1bfde50502b462ed5c4173c63
Saurav Panthee <saurav.panthee@gmail.com>
2021-02-17 12:46:01 -0600

    Fixed the notification issue

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,33 +1,31 @@
-import React, { Fragment } from 'react'
-import PropTypes from 'prop-types'
-import SidebarItem from './SidebarItem'
-import SidebarWrapper from './SidebarWrapper'
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+import SidebarItem from "./SidebarItem";
+import SidebarWrapper from "./SidebarWrapper";
 
-function Sidebar({data}) {
-    return (
-        <Fragment>
-            <SidebarWrapper>
-
-           
-    {data && data.length > 0 ? (
+function Sidebar({ data }) {
+  return (
+    <Fragment>
+      <SidebarWrapper>
+        {data && data.length > 0 ? (
           data.map((item) =>
-            // item.type !== "StudentEnrollment" ? 
-            (
-              <SidebarItem key  ={item.id} data ={item.assignment.name} ></SidebarItem>
-            ) 
-            // : null
+            item.type !== "StudentEnrollment" ? (
+              <SidebarItem
+                key={item.id}
+                data={item.assignment.name}
+              ></SidebarItem>
+            ) : null
           )
         ) : (
           <h2>No Todo Found</h2>
         )}
-         </SidebarWrapper>
- 
-        </Fragment>
-    )
+      </SidebarWrapper>
+    </Fragment>
+  );
 }
 
 Sidebar.propTypes = {
-data: PropTypes.object.isRequired,
-}
+  data: PropTypes.object.isRequired,
+};
 
-export default Sidebar
+export default Sidebar;
